6a991b75587c861e0439ee3f69d88cae384f008a
Güneş Pirinçoğlu <gp01prc@gmail.com>
2025-10-18 21:39:07 +0300

    Add files via upload

---
+++ urunler.js
@@ -0,0 +1,42 @@
+// urunler.js
+
+const urunler = {
+  koltuk1: {
+    baslik: "Koltuk Takımı",
+    fiyat: "₺8.500",
+    aciklama: "Deneme açıklaması",
+    resimler: [
+      "Resimler/trobio.jpg",
+      "Resimler/babyhornet.png"
+    ]
+  },
+
+  koltuk2: {
+    baslik: "Koltuk Takımı",
+    fiyat: "₺99.50000",
+    aciklama: "2.link",
+    resimler: [
+      "Resimler/trobio.jpg",
+      "Resimler/babyhornet.png"
+    ]
+  },
+
+  aksesuar1: {
+    baslik: "Duvar Saati",
+    fiyat: "₺750",
+    aciklama: "Şık ve sessiz mekanizmalı duvar saati, modern tasarımıyla her ortama uyum sağlar.",
+    resimler: [
+      "Resimler/trobio.jpg",
+      "Resimler/entrance.png"
+    ]
+  },
+  salon1: {
+    baslik: "Salon Takımı",
+    fiyat: "₺15.000",
+    aciklama: "Şık tasarımı ve dayanıklı malzemesiyle bu salon takımı, evinize modern bir hava katar.",
+    resimler: [
+      "Resimler/trobio.jpg",
+      "Resimler/babyhornetmom.png"
+    ]
+  }
+};
